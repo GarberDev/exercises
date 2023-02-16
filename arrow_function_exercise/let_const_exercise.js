e4e9a45eca641ccc471d6d4533916850f031fb6a
Justin Garber <JustinGarber@outlook.com>
2023-02-16 00:00:21 -0800

    arrow function c

---
+++ arrow_function_exercise/let_const_exercise.js
@@ -0,0 +1,18 @@
+//var PI = 3.14;
+/// PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+
+//- What is the difference between ***var*** and ***let***
+
+//var is not block scoped, let is, var creates a global variable, let does not
+//var is not redecalred, let is
+
+//- What is the difference between ***var*** and ***const***?
+//var is not block scoped, const is, var creates a global variable, const does not
+
+//- What is the difference between let and const ?
+//var is not block scoped, let is, let is reassignable, const is not
+
+//- What is hoisting?
+//hoisting is when a variable is declared before it is used, but the value is not assigned until later
