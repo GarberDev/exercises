f717ec811926f287053c6602a68aec97b8053c00
Justin Garber <JustinGarber@outlook.com>
2023-02-17 03:38:44 -0800

    OO_exerciseq

---
+++ OO_exercise/car_oo_challenge.js
@@ -0,0 +1,58 @@
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  //Part One
+
+  honk() {
+    return "Beep.";
+  }
+
+  toString() {
+    return `The vehicle is a ${this.make} ${this.model} from ${year}`;
+  }
+}
+
+//Part Two
+
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
+}
+
+//Part Three
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+    this.revEngine = function () {
+      return "VROOM!!!";
+    };
+  }
+}
+
+//Part Four
+
+class Garage {
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    }
+    if (this.vehicles.length >= this.capacity) {
+      return "Sorry, we're full.";
+    }
+    this.vehicles.push(newVehicle);
+    return "Vehicle added!";
+  }
+}
