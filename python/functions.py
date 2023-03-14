b81a1bdbf7e0d4466e978ed9993c97e43829e8d9
Justin Garber <JustinGarber@outlook.com>
2023-03-13 18:33:52 -0700

    remove jeopardy

---
+++ python/functions.py
@@ -0,0 +1,10 @@
+# def greet(person):
+#     return f"Hello {person}"
+
+def divide(a,b):
+    if type(a) is int and type(b) is int:
+        return a/b
+    return 'a and b must be integer!'
+
+
+    
---
+++ python/loops.py
@@ -0,0 +1,20 @@
+# num = 0
+
+# while num <= 100:
+#     print(num)
+#     num = num+10
+
+# print("all done")
+
+target = 37
+
+guess =  None
+
+while guess != target:
+    guess=input("Please enter a guess....")
+    guess =int(guess)
+
+
+
+
+print("all done")
