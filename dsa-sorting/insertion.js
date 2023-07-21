6ba3dfd124e209c5d1c867060bdc43f58bfb771f
Justin Garber <JustinGarber@outlook.com>
2023-07-20 20:35:26 -0700

    sorting

---
+++ dsa-sorting/insertion.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+  let currentVal;
+  for (let i = 1; i < arr.length; i++) {
+    currentVal = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > currentVal) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+module.exports = insertionSort;
